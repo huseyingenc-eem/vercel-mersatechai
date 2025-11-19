aa22feb60d30b7c795305206b2624562b10e6f62
Hüseyin Genç <huseyingenc.eem@gmail.com>
2025-11-19 17:07:07 +0300

    card-stack yapısı düzeltildi

--- components/ui/card-stack.tsx
+++ components/ui/card-stack.tsx
@@ -10,12 +10,10 @@ export const CardStack = ({
   items,
   offset,
   scaleFactor,
-  className,
 }: {
   items: Card[];
   offset?: number;
   scaleFactor?: number;
-  className?: string;
 }) => {
   const CARD_OFFSET = offset || 12;
   const SCALE_FACTOR = scaleFactor || 0.06;
@@ -25,19 +23,19 @@ export const CardStack = ({
   }
 
   return (
-    <div className={"relative h-[450px] w-[340px] sm:w-[384px] " + (className || "")}>
+    <div className="relative h-[450px] w-[340px] sm:w-[384px]">
       {items.map((card, index) => {
         return (
           <motion.div
             key={card.id}
-            className="absolute inset-0 rounded-2xl shadow-xl border border-neutral-200 dark:border-neutral-800 bg-white dark:bg-neutral-900"
+            className="absolute dark:bg-neutral-900 bg-white h-[450px] w-[340px] sm:w-[384px] rounded-2xl shadow-xl border border-neutral-200 dark:border-neutral-800"
             style={{
               transformOrigin: "top center",
             }}
             initial={{
-              top: index * -CARD_OFFSET,
-              scale: 1 - index * SCALE_FACTOR,
-              zIndex: items.length - index,
+                top: index * -CARD_OFFSET,
+                scale: 1 - index * SCALE_FACTOR,
+                zIndex: items.length - index,
             }}
             animate={{
               top: index * -CARD_OFFSET,
@@ -45,11 +43,11 @@ export const CardStack = ({
               zIndex: items.length - index,
             }}
             transition={{
-              duration: 0.4,
-              ease: "circOut",
+                duration: 0.4,
+                ease: "circOut"
             }}
           >
-            <div className="h-full w-full overflow-hidden relative">{card.content}</div>
+            {card.content}
           </motion.div>
         );
       })}
